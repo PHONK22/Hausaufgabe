407953f098c82347da46840bffa50766f3e10f17
PHONK22 <sd4129@yandex.ru>
2022-05-26 22:09:15 +0200

    first commit

---
+++ Desktop/Lesson/lesson1/lesson2/lesson2.js
@@ -0,0 +1,56 @@
+/* NUMBER 1
+
+for(let i=10; i<=50; i++) {
+    console.log(i)
+}
+*/
+
+/* NUMBER 2
+const Ich= {
+    firstName:'Daria',
+    secondName:'Samokhina',
+    year: 23,
+    pets: false
+
+};
+
+console.log(Ich.year)
+*/
+
+/*NUMBER 3
+const array = [
+    'я в Симбирск,',
+    'в трактире.',
+    'с утра',
+    'В ту же ночь',
+    'Я остановился',
+    'для закупки', 
+    'что и было поручено Савельичу.',
+    'приехал,',
+    'где должен был',
+    'нужных вещей',
+    'отправился по лавкам',
+    'пробыть сутки',
+    'Савельич'
+] 
+const result = array [3]+ array[7] +array[0]+ array[8]+ array[11]+ array[5]+ array[9]+array[6]+array[4]+array[1]+array[12]+array[2]+array[10];
+
+console.log(result)
+*/
+
+/* NUMBER 4
+const showName = (firstName, lastName) => {
+  const fullName = `${firstName} ${lastName}`;
+  console.log(fullName);
+};
+showName("Daria", "Samokhina");
+  */
+
+/* NUMBER 5
+  let i=21;
+  while (i< 67){
+      i=i+2;
+      console.log(i);
+  }
+  
+  */
